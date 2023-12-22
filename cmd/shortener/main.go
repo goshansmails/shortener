5bc5551b5c240bc78bccf1153d40fec1d95dd12a
george pogosyan <goshansmails@gmail.com>
2023-12-22 20:02:20 +0300

    [iter1] Add hello-world server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func empty(resp http.ResponseWriter, req *http.Request) {
+	resp.Write([]byte("hello"))
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, empty)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		fmt.Printf("cat't run server: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
