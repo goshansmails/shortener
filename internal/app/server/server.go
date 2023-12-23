2ab24902958e3958bd2ad34f4fd8422ee6ccb513
george pogosyan <goshansmails@gmail.com>
2023-12-23 20:53:56 +0300

    [iter2] Refactor

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,11 +1,7 @@
 package server
 
 import (
-	"fmt"
-	"io"
 	"net/http"
-	"strconv"
-	"strings"
 
 	"github.com/goshansmails/shortener/internal/app/store"
 )
@@ -17,76 +13,6 @@ type Server struct {
 func New(store store.Store) *Server {
 	return &Server{
 		store: store,
-	}
-}
-
-const linkFormat = "http://localhost:8080/%d"
-
-func wrappedGetIDHandler(s *Server) func(resp http.ResponseWriter, req *http.Request) {
-
-	return func(resp http.ResponseWriter, req *http.Request) {
-
-		if req.URL.Path != "/" {
-			resp.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		body, err := io.ReadAll(req.Body)
-		if err != nil {
-			resp.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		urlToSave := string(body)
-		id, err := s.store.GetID(urlToSave)
-		if err != nil {
-			resp.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		resp.Header().Set("Content-Type", "text/plain")
-		resp.WriteHeader(http.StatusCreated)
-		resp.Write([]byte(fmt.Sprintf(linkFormat, id)))
-	}
-}
-
-func wrappedGetURLHandler(s *Server) func(resp http.ResponseWriter, req *http.Request) {
-
-	return func(resp http.ResponseWriter, req *http.Request) {
-
-		path := strings.Trim(req.URL.Path, "/")
-		id, err := strconv.Atoi(path)
-		if err != nil {
-			resp.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		url, err := s.store.GetURL(id)
-		if err != nil {
-			resp.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		resp.Header().Set("Location", url)
-		resp.WriteHeader(http.StatusTemporaryRedirect)
-	}
-}
-
-func mainHandlerWrapped(s *Server) func(resp http.ResponseWriter, req *http.Request) {
-
-	getIDHandler := wrappedGetIDHandler(s)
-	getURLHandler := wrappedGetURLHandler(s)
-
-	return func(resp http.ResponseWriter, req *http.Request) {
-
-		switch req.Method {
-		case http.MethodPost:
-			getIDHandler(resp, req)
-		case http.MethodGet:
-			getURLHandler(resp, req)
-		default:
-			resp.WriteHeader(http.StatusBadRequest)
-		}
 	}
 }
 
