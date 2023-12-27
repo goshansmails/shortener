f3deda79b3fdf005cd8ffba09e192e77d089c99c
george pogosyan <goshansmails@gmail.com>
2023-12-28 01:30:54 +0300

    [iter5] Adopt tests

--- internal/app/server/longer_url_test.go
+++ internal/app/server/longer_url_test.go
@@ -1,65 +1,13 @@
 package server
 
 import (
-	"io"
 	"net/http"
 	"net/http/httptest"
-	"regexp"
-	"strings"
 	"testing"
 
 	"github.com/goshansmails/shortener/internal/app/store/mockstore"
 	"github.com/stretchr/testify/require"
 )
-
-var linkRegExp = regexp.MustCompile("http://localhost:8080/[0-9A-Za-z]+")
-
-func TestGetIDHandler(t *testing.T) {
-	type TestCase struct {
-		name string
-		path string
-		ok   bool
-	}
-
-	tests := []TestCase{
-		{
-			name: "success",
-			path: "/",
-			ok:   true,
-		},
-		{
-			name: "bad path",
-			path: "/a/b/c",
-			ok:   false,
-		},
-	}
-
-	getIDHandler := wrappedGetIDHandler(mockstore.New(), "http://localhost:8080")
-
-	for _, test := range tests {
-
-		t.Run(test.name, func(t *testing.T) {
-			req := httptest.NewRequest(http.MethodPost, test.path, strings.NewReader("https://ya.ru"))
-			w := httptest.NewRecorder()
-			getIDHandler(w, req)
-
-			resp := w.Result()
-			defer resp.Body.Close()
-
-			if test.ok {
-				require.Equal(t, http.StatusCreated, resp.StatusCode)
-				require.Equal(t, "text/plain", resp.Header.Get("Content-Type"))
-
-				body, err := io.ReadAll(resp.Body)
-				require.NoError(t, err)
-				require.True(t, linkRegExp.MatchString(string(body)))
-
-			} else {
-				require.Equal(t, http.StatusBadRequest, resp.StatusCode)
-			}
-		})
-	}
-}
 
 func TestGetURLHandler(t *testing.T) {
 	store := mockstore.New()
@@ -99,14 +47,15 @@ func TestGetURLHandler(t *testing.T) {
 		},
 	}
 
-	getURLHandler := wrappedGetURLHandler(store)
+	s := New(Settings{Store: store})
 
 	for _, test := range tests {
 
 		t.Run(test.name, func(t *testing.T) {
 			req := httptest.NewRequest(http.MethodGet, test.path, nil)
 			w := httptest.NewRecorder()
-			getURLHandler(w, req)
+
+			s.LongerURL(w, req)
 
 			resp := w.Result()
 			defer resp.Body.Close()
---
+++ internal/app/server/shorten_url_test.go
@@ -0,0 +1,66 @@
+package server
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/goshansmails/shortener/internal/app/store/mockstore"
+	"github.com/stretchr/testify/require"
+)
+
+var linkRegExp = regexp.MustCompile("http://localhost:8080/[0-9A-Za-z]+")
+
+func TestGetIDHandler(t *testing.T) {
+	type TestCase struct {
+		name string
+		path string
+		ok   bool
+	}
+
+	tests := []TestCase{
+		{
+			name: "success",
+			path: "/",
+			ok:   true,
+		},
+		{
+			name: "bad path",
+			path: "/a/b/c",
+			ok:   false,
+		},
+	}
+
+	s := New(Settings{
+		Store:   mockstore.New(),
+		BaseURL: "http://localhost:8080",
+	})
+
+	for _, test := range tests {
+
+		t.Run(test.name, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodPost, test.path, strings.NewReader("https://ya.ru"))
+			w := httptest.NewRecorder()
+
+			s.ShortenURL(w, req)
+
+			resp := w.Result()
+			defer resp.Body.Close()
+
+			if test.ok {
+				require.Equal(t, http.StatusCreated, resp.StatusCode)
+				require.Equal(t, "text/plain", resp.Header.Get("Content-Type"))
+
+				body, err := io.ReadAll(resp.Body)
+				require.NoError(t, err)
+				require.True(t, linkRegExp.MatchString(string(body)))
+
+			} else {
+				require.Equal(t, http.StatusBadRequest, resp.StatusCode)
+			}
+		})
+	}
+}
